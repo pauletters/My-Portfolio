939a4ca00d3351699529ce2a0e455d382080b084
pauletters <140007528+pauletters@users.noreply.github.com>
2024-10-01 15:32:03 -0400

    Initial Commit

---
+++ Main/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header/header';
+import About from './components/body/about';
+
+function App() {
+  return (
+    <div className="header">
+      <Header />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ Main/src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Ryan Paulette</h1>
+        </header>
+    );
+}
+
+export default Header;
