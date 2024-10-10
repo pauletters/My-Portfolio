0bb3ad3f7ed914891c43bfc9626ba45c3f562640
pauletters <140007528+pauletters@users.noreply.github.com>
2024-10-10 13:37:00 -0400

    removed import index.css from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 
 // Render the App component to the root element in the DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
